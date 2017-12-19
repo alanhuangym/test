df243414de7d1b943512a4c506fe673207653b58
alanhuangym <alanhuangym@gmail.com>
2017-12-19 17:19:59 +0800

    fix

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import os
 def cout():
 	sum = 0
-	for i in range(i):
+	for i in range(10):
 		sum += i
 	print 'cout',sum
 cout()
 
 def pri():
 	sum = 0
-	for i in range(i):
+	for i in range(10):
 		sum += i
 	print 'cout',sum
 
