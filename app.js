d1bb153d79eb3a4e8f71c888f575adcb94185d2a
alanhuangym <alanhuangym@gmail.com>
2017-12-25 08:41:57 +0800

    change the project to node.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.end("Hello World\n");
+});
+//666666
+// Listen on port 8000, IP defaults to "0.0.0.0"
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server is running at http://127.0.0.1:8000/");
